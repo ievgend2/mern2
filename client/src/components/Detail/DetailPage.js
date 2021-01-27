398a299ea6909dcddab13afdb3ca1e2b45a56619
ievgend2 <ievgend2@gmail.com>
2021-01-26 22:27:24 -0500

    initialized routes

--- client/src/components/Detail/DetailPage.js
+++ client/src/components/Detail/DetailPage.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export const AuthPage = () => {
+export const DetailPage = () => {
     return (
         <div>
-            <h1>AuthPage</h1>
+            <h1>DetailPage</h1>
         </div>
     )
 }
